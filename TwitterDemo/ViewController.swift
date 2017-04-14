31c2957c7bf4d0a8cd470d984fb3456111e0db52
sideok you <sideokyou@sideokui-MacBook-Pro.local>
2017-04-14 16:03:07 -0700

    Initial Commit

---
+++ TwitterDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterDemo
+//
+//  Created by sideok you on 4/14/17.
+//  Copyright © 2017 sideok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
